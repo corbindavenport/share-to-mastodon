4de44b6cba0140df18544df2485bdd4f45160c2c
Corbin Davenport <me@corbin.io>
2022-12-18 13:47:46 -0500

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,98 @@
+// Initialize welcome message and context menu entry on extension load
+chrome.runtime.onInstalled.addListener(function (details) {
+	// Initialize context menu
+	chrome.contextMenus.create({
+		id: "share-to-mastodon",
+		title: 'Share to Mastodon',
+		contexts: ['selection', 'link', 'image', 'video', 'page']
+	})
+	// Show welcome message
+	if (details.reason === 'install' || details.reason === 'update') {
+		// Set message
+		const notification = {
+			type: 'basic',
+			iconUrl: chrome.runtime.getURL('img/icon_x128.png'),
+			title: 'Share to Mastodon ' + chrome.runtime.getManifest().version + ' installed!',
+			buttons: [
+				{
+					title: 'Open Settings'
+				},
+				{
+					title: 'Join Discord'
+				}
+			],
+			message: "Click here to see what's new in this version."
+		}
+		// Send notification
+		chrome.notifications.create(notification, () => {
+			// Handle notification click
+			chrome.notifications.onClicked.addListener(function () {
+				//chrome.tabs.create({ url: 'https://corbin.io/' })
+			})
+			// Handle notification button clicks
+			chrome.notifications.onButtonClicked.addListener(function (_, buttonIndex) {
+				if (buttonIndex === 0) {
+					chrome.runtime.openOptionsPage()
+				} else if (buttonIndex === 1) {
+					// Open Discord
+					chrome.tabs.create({ url: 'https://discord.com/invite/59wfy5cNHw' })
+				}
+			})
+		})
+	}
+})
+
+// Function for creating share popup
+function createPopup(serverDomain, shareLink, shareText, tab) {
+	var popupPage = 'https://' + serverDomain + '/share?text=' + encodeURIComponent(shareText + '\n\n' + shareLink)
+	var popupWidth = 500
+	var popupHeight = 500
+	var y = Math.round(tab.height / 2 - (popupHeight / 2))
+	var x = Math.round(tab.width / 2 - (popupWidth / 2))
+	console.log(popupWidth, popupHeight, y, x)
+	chrome.windows.create({
+		url: popupPage,
+		width: popupWidth,
+		height: popupHeight,
+		left: x,
+		top: y,
+		type: 'popup'
+	})
+}
+
+// Function for context menu search
+chrome.contextMenus.onClicked.addListener(async function (info, tab) {
+	console.log(info, tab)
+	if (info.menuItemId == "share-to-mastodon") {
+		// Check if there is a saved server
+		var server = await new Promise(function (resolve) {
+			chrome.storage.sync.get(function (data) {
+				resolve(data.userServer)
+			})
+		})
+		console.log('Retrieved server:', server)
+		// Open settings if needed
+		if (!server) {
+			chrome.runtime.openOptionsPage()
+			return false
+		}
+		// Set link and description
+		var shareLink = ''
+		var shareText = ''
+		if (info.mediaType) {
+			shareLink = info.srcUrl
+			shareText = tab.title
+		} else if (info.linkUrl) {
+			shareLink = info.linkUrl
+			shareText = 'Type something here'
+		} else if (info.selectionText) {
+			shareLink = info.pageUrl
+			shareText = '"' + info.selectionText + '"'
+		} else {
+			shareLink = info.pageUrl
+			shareText = 'Type something here'
+		}
+		// Open popup
+		createPopup(server, shareLink, shareText, tab)
+	}
+})
---
+++ js/settings.js
@@ -0,0 +1,33 @@
+// Function for populating settings
+async function loadSettings() {
+	// Retrieve settings from storage
+	const server = document.getElementById('mastodon-server')
+	new Promise(function (resolve) {
+		chrome.storage.sync.get(function (data) {
+			console.log(data)
+			// Server setting
+			if (data.userServer) {
+				server.value = data.userServer
+			} else {
+				document.querySelector('#mastodon-server-alert').classList.remove('d-none')
+			}
+			resolve()
+		})
+	}).then(function () {
+		// Allow interaction on settings
+		server.removeAttribute('disabled')
+	})
+}
+
+// Save settings after any input change
+document.querySelectorAll('input,select').forEach(function (el) {
+	el.addEventListener('change', function () {
+		chrome.storage.sync.set({
+			userServer: document.querySelector('#mastodon-server').value,
+		}, function() {
+			console.log('Settings saved')
+		})
+	})
+})
+
+loadSettings()
